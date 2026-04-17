80f8eb73a986bf24a008f4f75edee3b843ab765d
Veronika Ihnashkina <ihnasver@cvut.cz>
2026-04-17 13:57:02 +0200

    Added room.frag, room.vert

--- semestral_project/camera.cpp
+++ semestral_project/camera.cpp
@@ -1,6 +1,17 @@
 #include "camera.h"
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+glm::mat4 projMatrix;
+
 void initCamera() {
+    projMatrix = glm::perspective(
+        glm::radians(60.0f),
+        1280.0f / 720.0f,
+        0.1f, 100.0f
+    );
 }
 
 void setCameraStatic(int index) {
@@ -22,12 +33,17 @@ void onMouseClick(int button, int state, int x, int y) {
 }
 
 void updateProjection(int width, int height) {
+    projMatrix = glm::perspective(
+        glm::radians(60.0f),
+        (float)width / (float)height,
+        0.1f, 100.0f
+    );
 }
 
 glm::mat4 getViewMatrix() {
-    return glm::mat4(1.0f);
+    return glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 }
 
 glm::mat4 getProjectionMatrix() {
-    return glm::mat4(1.0f);
+    return projMatrix;
 }
